8b440aaca357ccdf33b8f45006c749ef56674c6e
Larry-Otwori <larry.akama@strathmore.edu>
2022-10-26 19:57:37 +0300

    version 1

---
+++ 1_output.py
@@ -0,0 +1,8 @@
+#output in python
+#The print () function is used to display output on the screen
+print ("Hello World") #brackets included because this is a function. notice that we do not terminate with semicolon in py
+#python statements have no terminator
+
+print(2+2)
+
+print(10%3)
---
+++ 2_datatypes.py
@@ -0,0 +1,37 @@
+#Datatypes in python
+
+# primitive datatypes
+from operator import truediv
+
+
+x = 5  # int data type
+y = 2.5  # float data type
+
+# declare variables on the same line
+#x,y = 5, 2.5
+
+# python allows for the assigning of same value to multiple variables
+a = b = c = 3
+
+my_string = {"Hello There"}  # str data type
+is_true = True  # Boolean data type
+is_false = False  # Boolean data type
+my_first_list = [1, 2, 3]  # List data type
+none_type = None
+# single quotation marks can also be used (inside curly braces)
+my_string = {'Hello There'}
+my_char = {'a'}
+
+# check for the data types of the variables
+print(type(x))
+print(type(y))
+print(type(a))
+print(type(my_string))
+print(type(is_true))
+print(type(my_first_list))
+print(type(none_type))
+print(type(my_char))  # take note of this in terms of output
+
+#my_string = {'Hello There'}
+
+print
